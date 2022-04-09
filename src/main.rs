7c4f4f4670724eade6b168805cd9c5a4f9874178
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-04-09 16:22:20 +0100

    Read the date

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, BufReader};
+use flate2::bufread::GzDecoder;
+use std::io::prelude::*;
+use std::fs::File;
+
+fn main() -> io::Result<()> {
+    let f = File::open("/home/pi/2022_place_canvas_history.csv.gzip")?;
+    let f = BufReader::new(f);
+
+    let mut gz = BufReader::new(GzDecoder::new(f));
+
+    for line in gz.lines() {
+        let line = line.unwrap();
+        let mut it = line.split(",");
+        let timestamp = it.next().unwrap();
+        println!("{}", timestamp);
+    }
+
+    Ok(())
 }
