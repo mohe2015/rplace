dc51e5bd6da5c149a3c556e9314d774d7da143a4
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-04-09 16:51:51 +0100

    Finish impl

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io::{self, BufReader};
 use flate2::bufread::GzDecoder;
 use std::io::prelude::*;
 use std::fs::File;
+use std::collections::HashMap;
 
 fn main() -> io::Result<()> {
     let f = File::open("/home/pi/2022_place_canvas_history.csv.gzip")?;
@@ -9,11 +10,22 @@ fn main() -> io::Result<()> {
 
     let mut gz = BufReader::new(GzDecoder::new(f));
 
-    for line in gz.lines() {
+    let mut next_user_id = -1;
+    let mut next_pixel_color = -1;
+    let mut user_ids = HashMap::new();
+    let mut pixel_colors = HashMap::new();
+    for line in gz.lines().skip(1) {
         let line = line.unwrap();
         let mut it = line.split(",");
         let timestamp = it.next().unwrap();
-        println!("{}", timestamp);
+        let user_id = it.next().unwrap();
+        let user_id = user_ids.entry(user_id.to_string()).or_insert_with(|| { next_user_id += 1; next_user_id });
+        let pixel_color = it.next().unwrap();
+        let pixel_color = pixel_colors.entry(pixel_color.to_string()).or_insert_with(|| { next_pixel_color += 1; next_pixel_color });
+        let coordinate_x = &it.next().unwrap()[1..];
+        let coordinate_y = it.next().unwrap();
+        let coordinate_y = &coordinate_y[..coordinate_y.len()-1];
+        println!("{},{},{},{},{}", timestamp, user_id, pixel_color, coordinate_x, coordinate_y);
     }
 
     Ok(())
