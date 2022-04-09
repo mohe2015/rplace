e10f287aaabdaf90325a08e3e596d71a6859b20b
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-04-09 19:47:34 +0100

    Jo

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,10 @@ use base64::{decode_config_slice, STANDARD};
 // 21714634193
 // ~22 GB
 
+// 1m46.864s
+
+// hex parsing: 
+
 fn main() -> io::Result<()> {
     let stdout = io::stdout();
     let mut handle = stdout.lock();
@@ -17,9 +21,9 @@ fn main() -> io::Result<()> {
     let mut gz = BufReader::new(GzDecoder::new(f));
 
     let mut next_user_id = -1;
-    let mut next_pixel_color = -1;
+    //let mut next_pixel_color = -1;
     let mut user_ids = FxHashMap::default();
-    let mut pixel_colors = FxHashMap::default();
+    //let mut pixel_colors = FxHashMap::default();
     let mut line = Vec::<u8>::new();
     let mut number_of_bytes_read = 0;
     gz.read_until(b'\n', &mut line)?; // drop header
@@ -34,7 +38,7 @@ fn main() -> io::Result<()> {
             eprintln!("{} %", f64::from(u32::try_from(number_of_bytes_read).unwrap()) / 21714634193f64);
         }
 
-        if number_of_bytes_read >= 2171463419 {
+        if number_of_bytes_read >= 2171463419 { // 10%
             break; // TESTING
         }
 
@@ -52,14 +56,48 @@ fn main() -> io::Result<()> {
             }
         };
         let pixel_color = u32::from_str_radix(std::str::from_utf8(&it.next().unwrap()[1..]).unwrap(), 16).unwrap();
-        // TODO maybe hex decode or store statically as the colors should be known
-        let pixel_color = match pixel_colors.get(&pixel_color) {
+        /*let pixel_color = match pixel_colors.get(&pixel_color) {
             Some(v) => *v,
             None => {
                 next_pixel_color += 1;
                 pixel_colors.insert(pixel_color, next_pixel_color);
                 next_pixel_color
             }
+        };*/
+        let pixel_color = match pixel_color {
+            52416 => 0,
+            0 => 5,
+            11815616 => 20,
+            16729344 => 25,
+            5368308 => 13,
+            16754688 => 27,
+            41832 => 7,
+            8461983 => 17,
+            13948889 => 22,
+            8318294 => 16,
+            9014672 => 18,
+            4799169 => 11,
+            16726145 => 24,
+            52344 => 8,
+            16766517 => 29,
+            16775352 => 30,
+            30063 => 6,
+            7161903 => 15,
+            10250534 => 19,
+            7143450 => 14,
+            6970623 => 2,
+            9745407 => 1,
+            14986239 => 4,
+            12451897 => 21,
+            14553215 => 23,
+            16757872 => 28,
+            16777215 => 31,
+            40618 => 3,
+            3576042 => 10,
+            2379940 => 9,
+            16751018 => 26,
+            5329490 => 12,
+            _ => panic!(),
         };
         let coordinate_x = std::str::from_utf8(&it.next().unwrap()[1..]).unwrap().parse::<u16>().unwrap();
         let coordinate_y = it.next().unwrap();
@@ -68,7 +106,7 @@ fn main() -> io::Result<()> {
         line.clear();
     }
 
-    eprintln!("{:#?}", pixel_colors);
+    //eprintln!("{:#?}", pixel_colors);
     eprintln!("user count: {}", next_user_id);
 
     Ok(())
