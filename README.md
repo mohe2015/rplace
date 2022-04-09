e731021abbd3c76173da41e947d860210f07b94b
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-04-09 17:48:47 +0100

    Before optimization work

---
+++ README.md
@@ -0,0 +1,6 @@
+# rplace
+
+RUSTFLAGS="-C target-cpu=native" cargo run --release
+
+cargo install flamegraph
+
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::{self, BufReader};
-use flate2::bufread::MultiGzDecoder;
+use flate2::bufread::GzDecoder;
 use std::io::prelude::*;
 use std::fs::File;
 use std::collections::HashMap;
@@ -8,7 +8,7 @@ fn main() -> io::Result<()> {
     let f = File::open("/home/pi/2022_place_canvas_history.csv.gzip")?;
     let f = BufReader::new(f);
 
-    let gz = BufReader::new(MultiGzDecoder::new(f));
+    let gz = BufReader::new(GzDecoder::new(f));
 
     let mut next_user_id = -1;
     let mut next_pixel_color = -1;
@@ -20,7 +20,7 @@ fn main() -> io::Result<()> {
         let timestamp = it.next().unwrap();
         let user_id = it.next().unwrap();
         let user_id = user_ids.entry(user_id.to_string()).or_insert_with(|| { next_user_id += 1; next_user_id });
-        let pixel_color = it.next().unwrap();
+        let pixel_color = &it.next().unwrap()[1..];
         let pixel_color = pixel_colors.entry(pixel_color.to_string()).or_insert_with(|| { next_pixel_color += 1; next_pixel_color });
         let coordinate_x = &it.next().unwrap()[1..];
         let coordinate_y = it.next().unwrap();
